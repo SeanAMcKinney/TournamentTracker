32205ee56e18f596e6f60d6f9dd1a85872cf7d30
Sean McKinney <seanamckinney8@gmail.com>
2022-08-04 15:14:59 -0500

    Finished up code for TextConnector.cs and TextConnectorProcessor.cs to allow TournamentDataFile to have an excell file with our data written to it.

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -19,16 +19,23 @@ namespace TrackerLibrary.DataAccess
             List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
 
             // ** Find the ID
-            int currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            int currentId = 1;
+
+            if (prizes.Count > 0)
+            {
+               currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
             model.Id = currentId;
 
             // **Add the new record with the new ID (max + 1)
             prizes.Add(model);
 
-           
-           
-           // Convert the prizes to list<string>
-           // Save the list<string> to the text file
+            // Convert the prizes to list<string>
+            // Save the list<string> to the text file
+            prizes.SaveToPrizeFile(PrizesFile);
+
+            return model;
         }
     }
 }
