bfddad22a05dc3a1d173dca9af77d712f1fd33e3
Sean McKinney <seanamckinney8@gmail.com>
2022-08-04 13:11:32 -0500

    Created TextConnectorProcessor.  Added  .TextConnector to the namespace because it is closely assciated with the TextConnector and provides file path uniquness to avoid duplication.  Wired up TextConnectorProcessor.

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -9,15 +9,13 @@ namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
-
         // TODO - Wire up the CreatePrize for text files.
-
         public PrizeModel CreatePrize(PrizeModel model)
         {
            // Load the text file
            // Convert the text to List<PrizeModel>
            // Find the ID
-           // Add the new record with thenew ID
+           // Add the new record with the new ID (max + 1)
            // Convert the prizes to list<string>
            // Save the list<string> to the text file
         }
---
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Load the text file
+// Convert the text to List<PrizeModel>
+// Find the ID
+// Add the new record with the new ID (max + 1)
+// Convert the prizes to list<string>
+// Save the list<string> to the text file
+
+namespace TrackerLibrary.DataAccess.TextConnector
+{
+    public static class TextConnectorProcessor
+    {
+        public static string FullFilePath(this string fileName) //PrizeModels.csv
+        {
+            // C:\Users\Student\Desktop\Tournament_Tracker\TournamentTracker\TournamentDataFile\PrizeModels.csv
+            return $"{ ConfigurationManager.AppSettings["filePath"] }\\{ fileName }";
+        }
+
+        public static List<string> LoadFile(this string file)
+        {
+            if (!File.Exists(file))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(file).ToList();
+        }
+    }
+}
