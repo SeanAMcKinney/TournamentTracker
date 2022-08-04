54f756eea8c06e5561ce282c743144381630a9c5
Sean McKinney <seanamckinney8@gmail.com>
2022-08-04 14:38:10 -0500

    added ConvertToPrizeModels() and code to TextConnectorProcessor.cs.

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,16 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
+        
         // TODO - Wire up the CreatePrize for text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
-           // Load the text file
-           // Convert the text to List<PrizeModel>
+            // ** Load the text file ** Convert the text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            int currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+           
            // Find the ID
            // Add the new record with the new ID (max + 1)
            // Convert the prizes to list<string>
--- TrackerLibrary/DataAccess/TextConnectorProcessor.cs
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -5,15 +5,16 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.Models;
 
-// Load the text file
-// Convert the text to List<PrizeModel>
+// ** Load the text file
+// ** Convert the text to List<PrizeModel>
 // Find the ID
 // Add the new record with the new ID (max + 1)
 // Convert the prizes to list<string>
 // Save the list<string> to the text file
 
-namespace TrackerLibrary.DataAccess.TextConnector
+namespace TrackerLibrary.DataAccess.TextHelpers
 {
     public static class TextConnectorProcessor
     {
@@ -31,6 +32,26 @@ namespace TrackerLibrary.DataAccess.TextConnector
             }
 
             return File.ReadAllLines(file).ToList();
+        }
+
+        public static List<PrizeModel> ConvertToPrizeModels(this List<string> lines)
+        {
+            List<PrizeModel> output = new List<PrizeModel>();
+
+            foreach (string line in lines)
+            {
+                string[] cols = line.Split(',');
+
+                PrizeModel p = new PrizeModel();
+                p.Id = int.Parse(cols[0]);
+                p.PlaceNumber = int.Parse(cols[1]);
+                p.PlaceName = cols[2];
+                p.PrizeAmount = decimal.Parse(cols[3]);
+                p.PrizePercentage = double.Parse(cols[4]);
+                output.Add(p);
+            }
+
+            return output;
         }
     }
 }
