3dd62d2240af92a82d85842531998a8be291d998
Sean McKinney <seanamckinney8@gmail.com>
2022-08-15 09:11:47 -0500

    added list of instructions for continuing createTournamentButton_Click() in CreateTournamentForm.cs.

--- TrackerUI/CreateTournamentForm.cs
+++ TrackerUI/CreateTournamentForm.cs
@@ -130,6 +130,10 @@ namespace TrackerUI
             tm.EnteredTeams = selectedTeams;
 
             // TODO - Wire up our matchups
+            // Order our list randomly of teams
+            // Check if it is big enough - if not, add in byes  2*2*2*2 (2^4) or 2^nth
+            // Create our first round of matchups
+            // Create every round after that - 8 mathcups - 4 mathcups - 2 mathcups - 1 matchup
 
             // Create Tournament entry
             // Create all of the prizes entries
