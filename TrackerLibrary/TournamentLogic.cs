474a21267137c3b79d55c944cf39e1cf91b7a1b9
Sean McKinney <seanamckinney8@gmail.com>
2022-08-15 09:19:07 -0500

    Added TournamentLogic.cs Class.  Create TournamentLogic.CreateRounds() in that class.  Called the method in the CreateTournmentButton_Click() in order to keep logic out of method.

---
+++ TrackerLibrary/TournamentLogic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.Models;
+
+namespace TrackerLibrary
+{
+    public static class TournamentLogic
+    {
+        // Order our list randomly of teams
+        // Check if it is big enough - if not, add in byes  2*2*2*2 (2^4) or 2^nth
+        // Create our first round of matchups
+        // Create every round after that - 8 mathcups - 4 mathcups - 2 mathcups - 1 matchup
+
+        public static void CreateRounds(TournamentModel model)
+        {
+
+        }
+    }
+}
--- TrackerUI/CreateTournamentForm.cs
+++ TrackerUI/CreateTournamentForm.cs
@@ -130,10 +130,7 @@ namespace TrackerUI
             tm.EnteredTeams = selectedTeams;
 
             // TODO - Wire up our matchups
-            // Order our list randomly of teams
-            // Check if it is big enough - if not, add in byes  2*2*2*2 (2^4) or 2^nth
-            // Create our first round of matchups
-            // Create every round after that - 8 mathcups - 4 mathcups - 2 mathcups - 1 matchup
+            TournamentLogic.CreateRounds(tm);
 
             // Create Tournament entry
             // Create all of the prizes entries
