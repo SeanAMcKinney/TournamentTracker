7a886094d1ca0937ebd820f3c1df3250365d27f3
Sean McKinney <seanamckinney8@gmail.com>
2022-08-19 14:14:19 -0500

    finished GetTourname_all() in SqlConnecto.cs.

--- TrackerLibrary/Models/MatchupEntryModel.cs
+++ TrackerLibrary/Models/MatchupEntryModel.cs
@@ -15,19 +15,34 @@ namespace TrackerLibrary.Models
         public int Id { get; set; }
 
         /// <summary>
+        /// The unique identifier fo the team.
+        /// </summary>
+
+        public int TeamCompetingId { get; set; }
+
+        /// <summary>
         /// Represents one team in the matchup.
         /// </summary>
+        /// 
         public TeamModel TeamCompeting { get; set; }
 
         /// <summary>
         /// Represents one score for this particular team.
         /// </summary>
+        /// 
         public double Score { get; set; }
+
+        /// <summary>
+        /// The unique identifier for the parent matchup (team).
+        /// </summary>
+
+        public int ParentMatchupId { get; set; }
 
         /// <summary>
         /// Represents the matchup that this team came
         /// from as the winner.
         /// </summary>
+        /// 
         public MatchupModel ParentMatchup { get; set; }       
     }
 }
